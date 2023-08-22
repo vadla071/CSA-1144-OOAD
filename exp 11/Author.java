47a1f87b828012721c0534d564578cb88cc2be0b
vadla071 <112850456+vadla071@users.noreply.github.com>
2023-08-22 10:43:22 +0530

    Add files via upload

---
+++ exp 11/Author.java
@@ -0,0 +1,106 @@
+
+
+/**
+ * Class acc0unt
+ */
+public class acc0unt {
+
+  //
+  // Fields
+  //
+
+  private int balance;
+  
+  //
+  // Constructors
+  //
+  public acc0unt () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of balance
+   * @param newVar the new value of balance
+   */
+  private void setBalance (int newVar) {
+    balance = newVar;
+  }
+
+  /**
+   * Get the value of balance
+   * @return the value of balance
+   */
+  private int getBalance () {
+    return balance;
+  }
+
+  //
+  // Other methods
+  //
+
+}
+
+
+
+/**
+ * Class Author
+ */
+public class Author {
+
+  //
+  // Fields
+  //
+
+  static private String name;
+  private String affilation;
+  
+  //
+  // Constructors
+  //
+  public Author () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Get the value of name
+   * @return the value of name
+   */
+  private String getName () {
+    return name;
+  }
+
+  /**
+   * Set the value of affilation
+   * @param newVar the new value of affilation
+   */
+  private void setAffilation (String newVar) {
+    affilation = newVar;
+  }
+
+  /**
+   * Get the value of affilation
+   * @return the value of affilation
+   */
+  private String getAffilation () {
+    return affilation;
+  }
+
+  //
+  // Other methods
+  //
+
+}
---
+++ exp 11/paper.java
@@ -0,0 +1,131 @@
+
+
+/**
+ * Class acc0unt
+ */
+public class acc0unt {
+
+  //
+  // Fields
+  //
+
+  private int balance;
+  
+  //
+  // Constructors
+  //
+  public acc0unt () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of balance
+   * @param newVar the new value of balance
+   */
+  private void setBalance (int newVar) {
+    balance = newVar;
+  }
+
+  /**
+   * Get the value of balance
+   * @return the value of balance
+   */
+  private int getBalance () {
+    return balance;
+  }
+
+  //
+  // Other methods
+  //
+
+}
+
+
+
+/**
+ * Class paper
+ */
+public class paper {
+
+  //
+  // Fields
+  //
+
+  private String title;
+  private String keywords;
+  private int state;
+  
+  //
+  // Constructors
+  //
+  public paper () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of title
+   * @param newVar the new value of title
+   */
+  private void setTitle (String newVar) {
+    title = newVar;
+  }
+
+  /**
+   * Get the value of title
+   * @return the value of title
+   */
+  private String getTitle () {
+    return title;
+  }
+
+  /**
+   * Set the value of keywords
+   * @param newVar the new value of keywords
+   */
+  private void setKeywords (String newVar) {
+    keywords = newVar;
+  }
+
+  /**
+   * Get the value of keywords
+   * @return the value of keywords
+   */
+  private String getKeywords () {
+    return keywords;
+  }
+
+  /**
+   * Set the value of state
+   * @param newVar the new value of state
+   */
+  private void setState (int newVar) {
+    state = newVar;
+  }
+
+  /**
+   * Get the value of state
+   * @return the value of state
+   */
+  private int getState () {
+    return state;
+  }
+
+  //
+  // Other methods
+  //
+
+}
