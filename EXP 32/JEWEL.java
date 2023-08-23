ac46adf2b91c5467be0ef6517b9ed410baa4be36
vadla071 <112850456+vadla071@users.noreply.github.com>
2023-08-23 08:34:11 +0530

    Add files via upload

---
+++ EXP 32/JEWEL.java
@@ -0,0 +1,111 @@
+
+
+/**
+ * Class acc0unt
+ */
+public class acc0unt {
+
+  //
+  // Fields
+  //
+
+  private int balance;
+  
+  //
+  // Constructors
+  //
+  public acc0unt () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of balance
+   * @param newVar the new value of balance
+   */
+  private void setBalance (int newVar) {
+    balance = newVar;
+  }
+
+  /**
+   * Get the value of balance
+   * @return the value of balance
+   */
+  private int getBalance () {
+    return balance;
+  }
+
+  //
+  // Other methods
+  //
+
+}
+
+
+
+/**
+ * Class JEWEL
+ */
+public class JEWEL {
+
+  //
+  // Fields
+  //
+
+  private void JEWEL_;
+  
+  //
+  // Constructors
+  //
+  public JEWEL () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of JEWEL_
+   * @param newVar the new value of JEWEL_
+   */
+  private void setJEWEL_ (void newVar) {
+    JEWEL_ = newVar;
+  }
+
+  /**
+   * Get the value of JEWEL_
+   * @return the value of JEWEL_
+   */
+  private void getJEWEL_ () {
+    return JEWEL_;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void JEWEL_ID()
+  {
+  }
+
+
+  /**
+   */
+  public void JEWEL_ADDRESS()
+  {
+  }
+
+
+}
